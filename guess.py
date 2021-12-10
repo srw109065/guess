5d916817cea7f91e7cc9b30c3d5c12738ce3db42
Dio <srw109065@gmail.com>
2021-12-10 21:16:05 +0800

    讓使用者重複猜測，並提示

--- guess.py
+++ guess.py
@@ -1,20 +1,17 @@
 import random
 ra = random.randint(1,5)
 ra = int(ra)
-times = 3
-times = int(times)
-print(ra)
 
-while times > 0:
-	times -= 1
+
+while True:
 	guess = input("請你猜數字: ")
 	guess = int(guess)
 	if guess == ra:
 		print("猜對囉")
 		break
 	else:
-		if times == 0:
-			print("遊戲結束")
-		elif times > 0:
-			print("猜錯囉", times, "次機會")
+		if ra > guess:
+			print("猜小了")
+		elif ra < guess:
+			print("猜大了")
 		
