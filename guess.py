73d1c213c5182d571818e8db108056c4ea37ed2a
Dio <srw109065@gmail.com>
2021-12-10 21:39:20 +0800

    更新_共猜幾次

--- guess.py
+++ guess.py
@@ -1,17 +1,20 @@
 import random
 ra = random.randint(1,5)
 ra = int(ra)
-
+times = 0
 
 while True:
 	guess = input("請你猜數字: ")
 	guess = int(guess)
 	if guess == ra:
 		print("猜對囉")
+		print("總共猜", times, "次")
 		break
 	else:
 		if ra > guess:
 			print("猜小了")
+			times += 1
 		elif ra < guess:
 			print("猜大了")
+			times += 1
 		
